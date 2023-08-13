c5c56022debfdc80b38dd5111c68a2263be0a072
omarfarukpial <omarfarukpial1@gmail.com>
2023-08-13 22:09:37 +0600

    Final update

---
+++ client/src/layout.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Navbar from "./navbar";
+import Home from "./pages/Home";
+
+export default function Layout() {
+  return (
+    <div className=" max-w-7xl p-3 mx-auto">
+        <Navbar/>
+        <Home/>
+        <Footer/>
+    </div> 
+  )
+}
+
+
